3a5d8fdfdbd846872e6aeb651e172c840fa6fc9b
Mykhailo Safronov <isevenpwnz@gmail.com>
2025-05-29 14:57:50 +0300

    add movie listing and detail endpoints with pagination support

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,52 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+import math
+
+from fastapi import APIRouter, Depends, HTTPException, Query, status, Request
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
-
+from schemas import MovieListResponseSchema, MovieDetailResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/")
+async def get_movie_list(
+        request: Request,
+        db: AsyncSession = Depends(get_db),
+        page: int = Query(default=1, ge=1),
+        per_page: int = Query(default=10, ge=1, le=20)
+) -> MovieListResponseSchema:
+    result = await db.execute(select(func.count(MovieModel.id)))
+    total_items = result.scalar()
+    if not total_items:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No movies found.")
+    total_pages = math.ceil(total_items / per_page)
+    offset = (page - 1) * per_page
+    result = await db.scalars(select(MovieModel).offset(offset).limit(per_page))
+    movies = result.all()
+    if not movies:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No movies found.")
+    base_url = request.url.path
+    prev_page = (
+        f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
+    )
+    next_page = (
+        f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
+    )
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items
+    )
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie_by_id(movie_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = result.scalar_one_or_none()
+    if not movie:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie with the given ID was not found.")
+    return MovieDetailResponseSchema.model_validate(movie)
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,73 @@
-# Write your code here
+from datetime import date
+
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseMovieSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    name: str
+    date: date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+    # ToDo test it than
+    # @field_validator('date')
+    # @classmethod
+    # def validate_date(cls, value: date) -> date:
+    #     current_year = date.today().year
+    #
+    #     if value.year > current_year:
+    #         raise ValidationError(f"Movie's year cannot be grater than {current_year}")
+    #
+    #     return value
+
+
+class MovieDetailResponseSchema(BaseMovieSchema):
+    id: int
+
+
+class MovieListResponseSchema(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={
+            "examples": [
+                {
+                    "movies": [
+                        {
+                            "id": 1,
+                            "name": "Creed III",
+                            "date": "2023-03-02",
+                            "score": 73,
+                            "genre": "Drama,Action",
+                            "overview": "After dominating the boxing world, Adonis Creed has been thriving in both "
+                                        "his career and family life...",
+                            "crew": "Michael B. Jordan, Tessa Thompson",
+                            "orig_title": "Creed III",
+                            "status": "Released",
+                            "orig_lang": "English",
+                            "budget": 75000000,
+                            "revenue": 271616668,
+                            "country": "AU"
+                        }
+                    ],
+                    "prev_page": "/theater/movies/?page=1&per_page=10",
+                    "next_page": "/theater/movies/?page=3&per_page=10",
+                    "total_pages": 1000,
+                    "total_items": 9999
+                }
+            ]
+        }
+    )
+    movies: list[MovieDetailResponseSchema]
+    prev_page: str | None = None
+    next_page: str | None = None
+    total_pages: int
+    total_items: int
